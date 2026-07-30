e18d6f51b28a06dd1342b086211939376fa75661
Sanguing <jan234550@gmail.com>
2026-07-30 19:17:38 +0200

    added few js scripts, contact & footer section

--- Kurs Tworzenia Stron WWW cz3/flexbox_proj/js/main.js
+++ Kurs Tworzenia Stron WWW cz3/flexbox_proj/js/main.js
@@ -2,14 +2,17 @@ const nav = document.querySelector(".nav")
 const navBtn = document.querySelector(".burger-btn")
 const allNavItems = document.querySelectorAll(".nav__item")
 
+const navBtnBars = document.querySelector(".burger-btn__bars")
+const allSections = document.querySelectorAll(".section")
+const footerYear = document.querySelector(".footer__year")
+
 const handleNav = () => {
 	const isOpening = !nav.classList.contains("nav--active")
 	nav.classList.toggle("nav--active")
+	navBtnBars.classList.remove("black-bars-color")
 
 	handleNavItemsAnimations()
 }
-
-navBtn.addEventListener("click", handleNav)
 
 allNavItems.forEach((item) => {
 	item.addEventListener("click", () => {
@@ -28,3 +31,30 @@ const handleNavItemsAnimations = () => {
 		delayTime += 0.1
 	})
 }
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add("black-bars-color")
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove("black-bars-color")
+		}
+	})
+}
+
+handleCurrentYear()
+window.addEventListener("scroll", handleObserver)
+navBtn.addEventListener("click", handleNav)
